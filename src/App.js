51f0710183375ebec5825d300a95de58a9764109
xnayn00 <yan_mendes.18@outlook.com>
2021-10-12 13:53:07 -0300

    Include fontawesome dependecies

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
+import Header from './components/header'
+import ProductsList from './components/productsList'
+import ProductsView from './components/productsView'
+import './css/main.css'
+
 function App() {
   return (
     <div className="App">
-      <h1>Começando o projeto da Sambay!</h1>
+      <Header />
+      <ProductsView />
     </div>
   );
 }
